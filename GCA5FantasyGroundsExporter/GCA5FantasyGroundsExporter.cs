32832ca6059d1702b089f20239aa95bae096440c
Pierre Waldfried <Pierre.Waldfried@outlook.de>
2022-01-24 20:51:16 +0100

    Fixed Warnings and is regocnized by GCA

--- GCA5FantasyGroundsExporter/GCA5FantasyGroundsExporter.cs
+++ GCA5FantasyGroundsExporter/GCA5FantasyGroundsExporter.cs
@@ -11,6 +11,7 @@ using System.Text.RegularExpressions;
 //when developing within Visual Studio for VB projects.
 using GCA5Engine;
 using System.Drawing;
+using System.Globalization;
 using System.Reflection;
 using GCA5.Interfaces;
 
@@ -19,6 +20,10 @@ namespace GCA5FantasyGroundsExporter
     public class GCA5FantasyGroundsExporter : GCA5.Interfaces.IExportSheet
     {
         public event IExportSheet.RequestRunSpecificOptionsEventHandler RequestRunSpecificOptions;
+
+        private const string PLUGINVERSION = "1.0.0.1";
+        private SheetOptionsManager myOptions;
+        //private List<Skill> Skills;
 
         public string PluginName()
         {
@@ -30,43 +35,131 @@ namespace GCA5FantasyGroundsExporter
             return "Export Character as PC to Fantasy Grunds";
         }
 
-        public void CreateOptions(SheetOptionsManager mySheetOptions)
-        {
-
-        }
-
-        public bool GenerateExport(Party Party, string TargetFilename, SheetOptionsManager Options)
-        {
-            throw new NotImplementedException();
-        }
-
-        
-
-        
-
         public string PluginVersion()
         {
-            throw new NotImplementedException();
-        }
-
-        public int PreferredFilterIndex()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool PreviewOptions(SheetOptionsManager Options)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string SupportedFileTypeFilter()
-        {
-            return "";
+            return PLUGINVERSION;
         }
 
         public void UpgradeOptions(SheetOptionsManager Options)
         {
-            throw new NotImplementedException();
+            //Not needed as of now
         }
+
+        public string SupportedFileTypeFilter()
+        {
+            return "XML files (*.xml)|*.xml";
+        }
+        public int PreferredFilterIndex()
+        {
+            //Only xml files are supported
+            return 0;
+        }
+
+        public void CreateOptions(SheetOptionsManager mySheetOptions)
+        {
+            SheetOption mySheetOption = new SheetOption();
+            SheetOptionDisplayFormat myDisplayFormat = new SheetOptionDisplayFormat();
+
+            myDisplayFormat.BackColor = SystemColors.Info;
+            myDisplayFormat.CaptionLocalBackColor = SystemColors.Info;
+
+            mySheetOption.Clear();
+            mySheetOption.Name = "Header_Description";
+            mySheetOption.Type = GCA5Engine.OptionType.Header;
+            mySheetOption.UserPrompt = PluginName() + PluginVersion();
+            mySheetOption.DisplayFormat = myDisplayFormat;
+            mySheetOptions.AddOption(mySheetOption);
+
+            mySheetOption.Clear();
+            mySheetOption.Name = "Description";
+            mySheetOption.Type = GCA5Engine.OptionType.Caption;
+            mySheetOption.UserPrompt = PluginDescription();
+            mySheetOption.DisplayFormat = myDisplayFormat;
+            mySheetOptions.AddOption(mySheetOption);
+
+        }
+
+        public bool PreviewOptions(SheetOptionsManager Options)
+        {
+            // As of now its not needed
+            return true;
+        }
+
+
+        public bool GenerateExport(Party Party, string TargetFilename, SheetOptionsManager Options)
+        {
+            myOptions = Options;
+
+            FileWriter ExportWriter = new FileWriter( );
+            ExportWriter.FileOpen(TargetFilename);
+            ExportPC(Party.Current, ExportWriter);
+
+            if (Party.Characters.Count > 20)
+            {
+                DialogOptions_RequestedOptions e = new DialogOptions_RequestedOptions();
+                SheetOptionsManager SOM = new GCA5Engine.SheetOptionsManager("RunSpecificOptions For " + PluginName());
+                e.RunSpecificOptions = SOM;
+                RequestRunSpecificOptions.Invoke(this, e);
+            }
+
+            try
+            {
+                ExportWriter.FileClose();
+            }
+            catch
+            {
+
+            }
+
+            return true;
+        }
+
+        private void ExportPC(GCACharacter myCharacter, FileWriter fileWriter )
+        {
+            fileWriter.Paragraph("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            fileWriter.Paragraph("<root release=\"4 | CoreRPG:3\" version=\"3.2\">");
+            fileWriter.Paragraph("<character>");
+            //Name
+            fileWriter.Paragraph("<name type=\"string\">" + myCharacter.Name + "</name>");
+
+            exportAbilities(myCharacter, fileWriter);
+
+            fileWriter.Paragraph("</character>");
+            fileWriter.Paragraph("</root>");
+        }
+
+        private void exportAbilities(GCACharacter myCharacter, FileWriter fileWriter)
+        {
+            fileWriter.Paragraph("<abilities>");
+            exportSkills(myCharacter, fileWriter);
+            exportSpells(myCharacter, fileWriter);
+            fileWriter.Paragraph("</abilities>");
+        }
+
+        private void exportSkills(GCACharacter myCharacter, FileWriter fileWriter)
+        {
+            var mySkills = myCharacter.ItemsByType[(int)TraitTypes.Skills];
+            int i = 1;
+
+            fileWriter.Paragraph("<skilllist>");
+            foreach (GCATrait skill in mySkills)
+            {
+                var index = "<id-" + i.ToString("D5", CultureInfo.CreateSpecificCulture("en-US")) + ">";
+                fileWriter.Paragraph(index);
+                fileWriter.Paragraph("<name type = \"string\">" + skill.FullNameTL + "</name>");
+                fileWriter.Paragraph("<type type=\"string\""+ skill.SkillType + "</type>");
+                fileWriter.Paragraph("<level type=\"number\"" + skill.Level + "</type>");
+                fileWriter.Paragraph(index.Insert(1,"/"));
+            }
+
+            fileWriter.Paragraph("</skilllist>");
+        }
+
+        private void exportSpells(GCACharacter myCharacter, FileWriter fileWriter)
+        {
+
+        }
+
     }
+
 }
