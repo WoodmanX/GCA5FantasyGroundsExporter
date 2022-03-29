001afa7c87381777c19fd9f37ea817419c2be3cb
Pierre Waldfried <Pierre.Waldfried@outlook.de>
2022-03-29 14:48:45 +0200

    Added Files for Compilation by GCA5

---
+++ GCA5FantasyGroundsExporter/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// In Projekten im SDK-Stil wie dem vorliegenden, bei dem verschiedene Assemblyattribute
+// üblicherweise in dieser Datei definiert wurden, werden diese Attribute jetzt während
+// der Builderstellung automatisch hinzugefügt und mit Werten aufgefüllt, die in den
+// Projekteigenschaften definiert sind. Informationen dazu, welche Attribute einbezogen
+// werden und wie dieser Prozess angepasst werden kann, finden Sie unter https://aka.ms/assembly-info-properties.
+
+
+// Wenn "ComVisible" auf FALSE festgelegt wird, sind die Typen in dieser Assembly
+// für COM-Komponenten nicht sichtbar. Wenn Sie von COM aus auf einen Typ in dieser
+// Assembly zugreifen müssen, legen Sie das ComVisible-Attribut für den betreffenden
+// Typ auf TRUE fest.
+
+
+[assembly: AssemblyTitle("Export PC To Fantasygrounds")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Export PC To Fantasygrounds")]
+[assembly: AssemblyCopyright("Copyright © 2021 WoodmanX")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+
